f9339a154801ff4c934db1d0eac81c274e51a5b3
James Blakeborough <JamesB435@hotmail.com>
2018-07-08 16:41:58 +0100

    Secret Keys for fetching data.

---
+++ src/Data/secretKeys.js
@@ -0,0 +1,9 @@
+
+///Keys for the FourSquare API
+
+export const clientId= "0ODEWIMASAEHSY12ZIJIVLMXT5IVTZQHIE42ERWYM15TL1SJ";
+export const clientSecret= "45ZH1TAQMZS2BGRU01WZKBUS0AUCUCDHYEIA10OCXICIX3PE";;
+
+///Key for the Google API
+
+export const googleKey = 'AIzaSyC0ACPMM0duKvHqmd1SMrRuT68d91OtONs';
