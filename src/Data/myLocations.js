1ae98c9d258deffec33b1535ee963d662a4b0be1
James Blakeborough <JamesB435@hotmail.com>
2018-07-09 16:09:47 +0100

    All my favorite places.

---
+++ src/Data/myLocations.js
@@ -0,0 +1,43 @@
+export const locations = [
+
+{
+                    'title': "Five Guys",
+                    'latitude': 53.7961623,
+                    'longitude': -1.5417112,
+                },
+                {
+                    'title': "Ciao Bella",
+                    'latitude': 53.7935046,
+                    'longitude': -1.5401753,
+                },
+                {
+                    'title': "Thai Aroy Dee",
+                    'latitude': 53.7999435,
+                    'longitude': -1.5391458,
+                },
+                {
+                    'title': "Town Hall Tavern",
+                    'latitude': 53.7995209,
+                    'longitude': -1.5512685,
+                },
+                {
+                    'title': "Akbar's",
+                    'latitude': 53.7988805,
+                    'longitude': -1.5382261,
+                },
+                {
+                    'title': "Jamie's Italian",
+                    'latitude': 53.7976018,
+                    'longitude': -1.5472712,
+                },
+                {
+                    'title': "Red's True Barbecue",
+                    'latitude': 53.7957446,
+                    'longitude': -1.5407107,
+                },
+                {
+                    'title': "Belgrave Music Hall and Canteen",
+                    'latitude': 53.8007561,
+                    'longitude': -1.5410204,
+                }
+];
